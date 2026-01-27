0a1d0758917bc8580185ad284e00f0833751041c
Talha Qamar <qamartalha@proton.me>
2026-01-27 08:46:38 +0500

    basic work

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 #include "raylib.h"
+
+enum class Cell {
+    Empty,
+    Bomb,
+}
+
+class Board {
+}
+
 int main(int argc, char** argv)
 {
     InitWindow(500, 500, "Crimesweeper");
